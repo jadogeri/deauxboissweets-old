f1a2de7d3175389a178814257cc8bdf4ceee03b4
Joseph Adogeri <joekadama@hotmail.com>
2024-08-29 14:29:12 -0500

    Added node / express project ** server **

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+const bodyParser = require("body-parser")
+const { corsOptions } = require("./config/cors")
+
+
+const app = express();
+
+const port = process.env.PORT || 5500;
+
+ 
+app.use(express.json());
+app.use(cors(corsOptions)) // Use this after the variable declaration
+app.use(bodyParser.json())
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
