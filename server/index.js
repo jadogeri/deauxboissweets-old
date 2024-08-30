75d137b244c744b96e05e32875937451540f6dda
jadogeri <jadogeri@uno.edu>
2024-08-30 07:26:43 -0500

    Added firebase dependencies

--- server/index.js
+++ server/index.js
@@ -4,9 +4,47 @@ const cors = require("cors");
 const bodyParser = require("body-parser")
 const { corsOptions } = require("./src/configs/cors")
 const { errorHandler } = require('./src/middlewares/errorHandler');
-
+const { initializeApp } = require("firebase/app")
+const { getFirestore, Firestore,getDocs, getDoc, collection } = require("firebase/firestore")
 
 dotenv.config();
+
+
+
+// Import the functions you need from the SDKs you need
+//import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const { firebaseConfig } = require("./src/configs/firebase")
+
+// Initialize Firebase
+const firebaseobj = initializeApp(firebaseConfig);
+
+//init Firestore
+
+const db = getFirestore(); 
+console.log(JSON.stringify(firebaseobj))
+
+
+// collection ref
+const colRef = collection(db, 'orders')
+
+
+getDocs(colRef)
+  .then(snapshot => {
+    // console.log(snapshot.docs)
+    let books = []
+    snapshot.docs.forEach(doc => {
+      books.push({ ...doc.data(), id: doc.id })
+    })
+    console.log(books)
+  })
+  .catch(err => {
+    console.log(err.message)
+  })
+
 
 const app = express();
 
